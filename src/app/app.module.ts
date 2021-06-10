f830bbceedb8d84f9e81a03504724832c78d4623
educhinski <educhinski650@gmail.com>
2021-06-10 07:35:08 +0300

    Initial layout setup, header, footer and carousel implementation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FeatureSliderComponent } from './components/feature-slider/feature-slider.component';
+import { ProductGridComponent } from './components/product-grid/product-grid.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureSliderComponent,
+    ProductGridComponent,
+    FooterComponent,
+    BannerComponent,
+    CarouselComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
